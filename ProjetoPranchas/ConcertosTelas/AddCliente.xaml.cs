2eec72973a0200c12e257eebbab1f48705e497be
Alisson <alissonchoffmann@gmail.com>
2018-08-30 21:46:38 -0300

    Finalizando Cadastrado de Campos da Tela AddCliente

--- ProjetoPranchas/ConcertosTelas/AddCliente.xaml.cs
+++ ProjetoPranchas/ConcertosTelas/AddCliente.xaml.cs
@@ -23,5 +23,10 @@ namespace ConcertosTelas
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
